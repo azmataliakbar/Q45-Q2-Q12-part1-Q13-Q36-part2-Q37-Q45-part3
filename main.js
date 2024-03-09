14b8959d3c096f25c7d191da238f010b21a7c72c
AzmatAliAkbar <144133389+azmataliakbar@users.noreply.github.com>
2024-03-09 15:00:41 +0500

    Add files via upload
    added hello world

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// print the following codes
+
+console.log("\nHello, World!");
+console.log("\nWelcome to Everyone.");
+console.log("\nBest Regards,\nRespected Gov. Sindh.");
---
+++ main.ts
@@ -0,0 +1,8 @@
+// print the following codes
+
+console.log("\nHello, World!");
+console.log("\nWelcome to Everyone.");
+console.log("\nBest Regards,\nRespected Gov. Sindh.");
+
+
+
